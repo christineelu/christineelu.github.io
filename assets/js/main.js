1d344ce1bc97a77bea7edfc26bd50188d020fa81
Christine Lu <clu@berkeley.edu>
2021-01-12 15:56:14 -0800

    complete initial website design

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+//source: https://stackoverflow.com/questions/9979827/change-active-menu-item-on-page-scroll 
+
+// Cache selectors
+var topMenu = $("#nav"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   // Set/remove active class
+   menuItems
+     .parent().removeClass("active")
+     .end().filter("[href='#"+id+"']").parent().addClass("active");
+    });
+
+var offset = $(':target').offset();
+var scrollto = offset.top +150; // minus fixed header height
+$('html, body').animate({scrollTop:scrollto}, 0);
